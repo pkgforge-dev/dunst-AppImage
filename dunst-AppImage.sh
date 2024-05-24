8f14ed8bd19a1c486bbbbae5af9488ca1bf05ff0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-05-24 03:32:20 -0400

    Update dunst-AppImage.sh

--- dunst-AppImage.sh
+++ dunst-AppImage.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+set -u
+ARCH=x86_64
 APP=dunst
 APPDIR="$APP.AppDir"
 SITE="dunst-project/dunst"
 EXEC="$APP"
+
+LINUXDEPLOY="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-static-x86_64.AppImage"
+APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')
 
 # CREATE DIRECTORIES
 [ -n "$APP" ] && mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
@@ -47,16 +52,8 @@ Categories=System
 Hidden=true
 EOF
 
-# MAKE APPIMAGE
-LINUXDEPLOY="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-static-x86_64.AppImage"
-cd .. && wget "$LINUXDEPLOY" -O linuxdeploy && chmod a+x ./linuxdeploy && ./linuxdeploy --appdir "$APPDIR" --executable "$APPDIR"/usr/bin/"$EXEC" --output appimage
+# MAKE APPIMAGE USING FUSE3 COMPATIBLE APPIMAGETOOL
+cd .. && wget "$LINUXDEPLOY" -O linuxdeploy && wget -q "$APPIMAGETOOL" -O ./appimagetool && chmod a+x ./linuxdeploy ./appimagetool \
+&& ./linuxdeploy --appdir "$APPDIR" --executable "$APPDIR"/usr/bin/"$EXEC" && VERSION="$APPVERSION" ./appimagetool -s ./"$APPDIR" || exit 1
 
-# LIBFUSE3
-APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')
-
-[ -n "$APPDIR" ] && ls *AppImage && rm -rf ./"$APPDIR" || exit 1
-./*AppImage --appimage-extract && mv ./squashfs-root ./"$APPDIR" && rm -f ./*AppImage && wget -q "$APPIMAGETOOL" -O ./appimagetool && chmod a+x ./appimagetool || exit 1
-
-# Do the thing!
-ARCH=x86_64 VERSION="$APPVERSION" ./appimagetool -s ./"$APPDIR" || exit 1
 [ -n "$APP" ] && mv ./*.AppImage .. && cd .. && rm -rf ./"$APP" && echo "All Done!" || exit 1
