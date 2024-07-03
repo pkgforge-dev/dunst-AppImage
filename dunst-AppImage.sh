0891f571fa0974475ccb1d7892afbaade4b26d37
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-07-03 07:19:55 -0400

    Update dunst-AppImage.sh

--- dunst-AppImage.sh
+++ dunst-AppImage.sh
@@ -8,14 +8,14 @@ SITE="dunst-project/dunst"
 EXEC="$APP"
 
 LINUXDEPLOY="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-static-x86_64.AppImage"
-APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')
+APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/[()",{} ]/\n/g' | grep -oi 'https.*continuous.*tool.*86_64.*mage$')
 
 # CREATE DIRECTORIES
 [ -n "$APP" ] && mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
 
 # DOWNLOAD AND BUILD ROFI
 CURRENTDIR="$(dirname "$(readlink -f "$0")")" # DO NOT MOVE THIS
-version=$(wget -q https://api.github.com/repos/"$SITE"/releases/latest -O - | sed 's/[()",{}]/ /g; s/ /\n/g' | grep 'https.*.dunst.*tarball' | head -1)
+version=$(wget -q https://api.github.com/repos/"$SITE"/releases/latest -O - | sed 's/[()",{} ]/\n/g' | grep 'https.*.dunst.*tarball' | head -1)
 wget "$version" -O download.tar.gz && tar fx ./*tar* && cd ./dunst* || exit 1
 WAYLAND=0 \
 SYSTEMD=0 \
