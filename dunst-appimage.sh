2626cbf8a9e0972aeb4c5b75418ac55fceae35df
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-11-02 03:13:37 -0400

    deploy libs manually

--- dunst-appimage.sh
+++ dunst-appimage.sh
@@ -3,33 +3,25 @@
 set -eu
 export ARCH="$(uname -m)"
 export APPIMAGE_EXTRACT_AND_RUN=1
-UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|dunst-AppImage|latest|*$ARCH.AppImage.zsync"
+UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|dunst|latest|*$ARCH.AppImage.zsync"
 APP=dunst
-APPDIR="$APP.AppDir"
 SITE="dunst-project/dunst"
-
 APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
-LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
-SHARUN="https://bin.ajam.dev/$(uname -m)/sharun"
+export COMPLETIONS=0
 
 # CREATE DIRECTORIES
-mkdir -p ./"$APP/$APPDIR"
-cd ./"$APP/$APPDIR"
+mkdir -p ./"$APP"/AppDir
+cd ./"$APP"/AppDir
 
 # DOWNLOAD
-HERE="$(dirname "$(readlink -f "$0")")" # DO NOT MOVE THIS
 version=$(wget -q https://api.github.com/repos/"$SITE"/releases/latest -O - \
 	| sed 's/[()",{} ]/\n/g' | grep 'https.*.dunst.*tarball' | head -1)
 wget "$version" -O download.tar.gz
 tar fx ./*tar* && cd ./dunst*
 
-export VERSION="$(echo $version | awk -F"/" '{print $(NF)}')"
-
 # BUILD DUNST
-export COMPLETIONS=0
-make PREFIX="$HERE"/shared
-make install PREFIX="$HERE"/shared
-
+make -j$(nproc) PREFIX=../usr
+make install PREFIX=../usr
 cd ..
 rm -rf ./dunst* ./download.tar.gz
 
@@ -37,37 +29,39 @@ rm -rf ./dunst* ./download.tar.gz
 cat >> ./AppRun << 'EOF'
 #!/bin/sh
 CURRENTDIR="$(dirname "$(readlink -f "$0")")"
-export PATH="$CURRENTDIR/bin:$PATH"
+export PATH="$CURRENTDIR/usr/bin:$PATH"
 [ -z "$APPIMAGE" ] && APPIMAGE="$0"
-
 BIN="${ARGV0#./}"
 unset ARGV0
-if [ -f "$CURRENTDIR/bin/$BIN" ]; then
-	exec "$CURRENTDIR/bin/$BIN" "$@"
+if [ -f "$CURRENTDIR/usr/bin/$BIN" ]; then
+	if [ "$BIN" = "dunstctl" ]; then
+		exec "$CURRENTDIR/usr/bin/$BIN" "$@"
+	else
+		exec "$CURRENTDIR/ld-musl-x86_64.so.1" "$CURRENTDIR/usr/bin/$BIN" "$@"
+	fi
 elif [ "$1" = "--notify" ]; then
 	shift
-	exec "$CURRENTDIR"/bin/dunstify "$@"
+	exec "$CURRENTDIR/ld-musl-x86_64.so.1" "$CURRENTDIR"/usr/bin/dunstify "$@"
 elif [ "$1" = "--ctl" ]; then
 	shift
-	exec "$CURRENTDIR"/bin/dunstctl "$@"
+	exec "$CURRENTDIR"/usr/bin/dunstctl "$@"
 else
 	if [ -z "$1" ]; then
 		echo "AppImage commands:"
 		echo " \"$APPIMAGE\"            runs dunst"
 		echo " \"$APPIMAGE --notify\"   runs dunstify"
 		echo " \"$APPIMAGE --ctl\"      runs dunstctl"
-		echo "You can also make symlinks to the AppImage with the names"
-		echo "dunstify and dunstctl and that will make it automatically"
-		echo "launch those binaries without needing to use extra flags"
+		echo "You can also make and run symlinks to the AppImage with the names"
+		echo "dunstify and dunstctl to launch them automatically without extra args"
 		echo "running dunst..."
 	fi
-	"$CURRENTDIR/bin/dunst" "$@"
+	exec "$CURRENTDIR/ld-musl-x86_64.so.1" "$CURRENTDIR/usr/bin/dunst" "$@"
 fi
 EOF
-chmod a+x ./AppRun
+chmod +x ./AppRun
 
 # Dummy Icon & Desktop
-touch ./dunst.png && ln -s ./dunst.png ./.DirIcon
+touch ./dunst.png && ln -s ./dunst.png ./.DirIcon # No official icon?
 cat >> ./"$APP.desktop" << 'EOF'
 [Desktop Entry]
 Type=Application
@@ -79,25 +73,28 @@ Hidden=true
 EOF
 
 # DEPLOY ALL LIBS
-mkdir -p ./bin
-mv ./shared/bin/dunstctl ./bin
-
-wget "$LIB4BN" -O ./lib4bin
-wget "$SHARUN" -O ./sharun
-chmod +x ./lib4bin ./sharun
-HARD_LINKS=1 ./lib4bin ./shared/bin/*
-rm -f ./lib4bin
+mkdir -p ./usr/lib
+ldd ./usr/bin/* | awk -F"[> ]" '{print $4}' | xargs -I {} cp -vf {} ./usr/lib
+if [ -f ./usr/lib/ld-musl-x86_64.so.1 ]; then
+	mv ./usr/lib/ld-musl-x86_64.so.1 ./
+else
+	cp /lib64/ld-musl-x86_64.so.1 ./
+fi
+find ./usr/bin -type f -exec patchelf --set-rpath '$ORIGIN/../lib' {} ';'
+find ./usr/lib -type f -exec patchelf --set-rpath '$ORIGIN' {} ';'
+find ./usr/lib ./usr/bin -type f -exec strip -s -R .comment --strip-unneeded {} ';'
 
 # Do the thing!
+export VERSION="$(./AppRun --version | awk '{print $(NF-1)}')"
 cd ..
 wget -q "$APPIMAGETOOL" -O appimagetool
 chmod +x ./appimagetool
+ls
 ./appimagetool --comp zstd \
 	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
-	-n -u "$UPINFO" ./"$APPDIR" dunst-"$VERSION"-"$ARCH".AppImage
-ls
+	-n -u "$UPINFO" ./AppDir ./dunst-"$VERSION"-"$ARCH".AppImage
 
-mv ./*.AppImage* ../ 
-cd .. 
+mv ./*.AppImage* ../
+cd ..
 rm -rf "$APP"
 echo "All Done!"
