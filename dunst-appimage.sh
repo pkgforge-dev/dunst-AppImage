0cb45380f02ce250753c4392db06debf8e4b80f2
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-21 19:12:11 -0400

    refactor

--- dunst-appimage.sh
+++ dunst-appimage.sh
@@ -1,47 +1,36 @@
 #!/bin/sh
 
-set -eu
+set -eux
+
 export ARCH="$(uname -m)"
 export APPIMAGE_EXTRACT_AND_RUN=1
+
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-APP=dunst
-SITE="dunst-project/dunst"
 APPIMAGETOOL="https://github.com/pkgforge-dev/appimagetool-uruntime/releases/download/continuous/appimagetool-$ARCH.AppImage"
-export COMPLETIONS=0
 
 # CREATE DIRECTORIES
-mkdir -p ./"$APP"/AppDir
-cd ./"$APP"/AppDir
+mkdir ./AppDir
+cd ./AppDir
 
-# DOWNLOAD
-version=$(wget -q https://api.github.com/repos/"$SITE"/releases/latest -O - \
-	| sed 's/[()",{} ]/\n/g' | grep 'https.*.dunst.*tarball' | head -1)
-wget "$version" -O download.tar.gz
-tar fx ./*tar* && cd ./dunst*
-
-# BUILD DUNST
-make -j$(nproc) PREFIX=../usr
-make install PREFIX=../usr
-cd ..
-rm -rf ./dunst* ./download.tar.gz
+mkdir ./etc/dunst
+cp -v /etc/xdg/dunst ./etc/dunst
 
 # AppRun
-cat >> ./AppRun << 'EOF'
-#!/bin/sh
+echo '#!/bin/sh
 CURRENTDIR="$(dirname "$(readlink -f "$0")")"
 export PATH="$CURRENTDIR/usr/bin:$PATH"
-[ -z "$APPIMAGE" ] && APPIMAGE="$0"
+[ -n "$APPIMAGE" ] || APPIMAGE="$0"
 BIN="${ARGV0#./}"
 unset ARGV0
 if [ -f "$CURRENTDIR/usr/bin/$BIN" ]; then
 	if [ "$BIN" = "dunstctl" ]; then
 		exec "$CURRENTDIR/usr/bin/$BIN" "$@"
 	else
-		exec "$CURRENTDIR/ld-musl-x86_64.so.1" "$CURRENTDIR/usr/bin/$BIN" "$@"
+		exec "$CURRENTDIR/ld-musl.so.1" "$CURRENTDIR/usr/bin/$BIN" "$@"
 	fi
 elif [ "$1" = "--notify" ]; then
 	shift
-	exec "$CURRENTDIR/ld-musl-x86_64.so.1" "$CURRENTDIR"/usr/bin/dunstify "$@"
+	exec "$CURRENTDIR/ld-musl.so.1" "$CURRENTDIR"/usr/bin/dunstify "$@"
 elif [ "$1" = "--ctl" ]; then
 	shift
 	exec "$CURRENTDIR"/usr/bin/dunstctl "$@"
@@ -55,49 +44,43 @@ else
 		echo "dunstify and dunstctl to launch them automatically without extra args"
 		echo "running dunst..."
 	fi
-	exec "$CURRENTDIR/ld-musl-x86_64.so.1" "$CURRENTDIR/usr/bin/dunst" "$@"
-fi
-EOF
+	exec "$CURRENTDIR/ld-musl.so.1" "$CURRENTDIR/usr/bin/dunst" "$@"
+fi' > ./AppRun
 chmod +x ./AppRun
 
 # Dummy Icon & Desktop
-touch ./dunst.png && ln -s ./dunst.png ./.DirIcon # No official icon?
-cat >> ./"$APP.desktop" << 'EOF'
-[Desktop Entry]
+touch ./dunst.png ./.DirIcon # No official icon?
+echo '[Desktop Entry]
 Type=Application
 Name=dunst
 Icon=dunst
 Exec=dunst
 Categories=System
-Hidden=true
-EOF
+Hidden=true' > dunst.desktop
 
 # DEPLOY ALL LIBS
-mkdir -p ./usr/lib
-ldd ./usr/bin/* /usr/lib/libnotify.so | awk -F"[> ]" '{print $4}' | xargs -I {} cp -vf {} ./usr/lib
-cp -vn /usr/lib/libnotify.so* ./usr/lib
-if [ -f ./usr/lib/ld-musl-x86_64.so.1 ]; then
-	mv ./usr/lib/ld-musl-x86_64.so.1 ./
-else
-	cp /lib64/ld-musl-x86_64.so.1 ./
-fi
-find ./usr/bin -type f -exec patchelf --set-rpath '$ORIGIN/../lib' {} ';'
-find ./usr/lib -type f -exec patchelf --set-rpath '$ORIGIN' {} ';'
-find ./usr/lib ./usr/bin -type f -exec strip -s -R .comment --strip-unneeded {} ';'
+mkdir -p ./usr/lib ./usr/bin
 
-# Do the thing!
+cp -v /usr/bin/dunst*             ./usr/bin
+cp -v /usr/lib/libnotify.so*      ./usr/lib
+cp -v /lib64/ld-musl-"$ARCH".so.1 ./ld-musl.so.1
+
+ldd ./usr/bin/* ./usr/lib/* \
+	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vf {} ./usr/lib
+
+find ./usr -type f -exec patchelf --set-rpath '$ORIGIN:$ORIGIN/../lib' {} ';'
+find ./usr -type f -exec strip -s -R .comment --strip-unneeded {} ';'
+
 export VERSION="$(./AppRun --version | awk 'FNR==1 {print $NF}')"
 [ -n "$VERSION" ]
 echo "$VERSION" > ~/version
-cd ..
-wget -q "$APPIMAGETOOL" -O appimagetool
-chmod +x ./appimagetool
-ls
-URUNTIME_PRELOAD=1 ./appimagetool --comp zstd \
-	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
-	-n -u "$UPINFO" ./AppDir ./dunst-"$VERSION"-anylinux-"$ARCH".AppImage
 
-mv ./*.AppImage* ../
+# Do the thing!
 cd ..
-rm -rf "$APP"
+wget "$APPIMAGETOOL" -O appimagetool
+chmod +x ./appimagetool
+URUNTIME_PRELOAD=1 \
+	./appimagetool -n -u \
+	"$UPINFO" ./AppDir ./dunst-"$VERSION"-anylinux-"$ARCH".AppImage
+
 echo "All Done!"
