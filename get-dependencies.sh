3dde84678c1f8a212505c878e3b52523a81e1f88
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-22 00:17:37 +0000

    refactor, make dedicated script for deps, fix syscofig file path (#8)

---
+++ get-dependencies.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -ex
+
+TARBALL=$(wget https://api.github.com/repos/dunst-project/dunst/releases/latest -O - \
+	| sed 's/[()",{} ]/\n/g' | grep 'https.*.dunst.*tarball' | head -1)
+
+export COMPLETIONS=0
+
+echo "Installing dependencies..."
+echo "---------------------------------------------------------------"
+apk add \
+	bash \
+	build-base \
+	coreutils \
+	dbus \
+	dbus-dev \
+	desktop-file-utils \
+	git \
+	libnotify-dev \
+	librsvg \
+	libxinerama-dev \
+	libxrandr-dev \
+	libxscrnsaver-dev \
+	pango-dev \
+	patchelf \
+	perl \
+	wayland-dev \
+	wayland-protocols \
+	wget
+
+echo "Building dunst..."
+wget "$TARBALL" -O download.tar.gz
+tar fx ./*.tar.* && (
+	cd ./dunst-project*
+	make -j$(nproc)
+	make install
+)
+rm -rf ./dunst-project* ./download.tar.gz
+
+echo "All done!"
+echo "---------------------------------------------------------------"
