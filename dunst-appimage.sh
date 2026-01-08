c6fcb8bce2115c749e74b28b778f7646d0538d13
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-08 02:30:42 -0400

    update to template

--- dunst-appimage.sh
+++ dunst-appimage.sh
@@ -1,91 +1,22 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
+ARCH=$(uname -m)
+VERSION=$(pacman -Q dunst | awk '{print $2; exit}') # example command to get version of application here
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export MAIN_BIN=dunst
+export ICON=DUMMY
+export DESKTOP=DUMMY
+export PATH_MAPPING='/etc/dunst:${SHARUN_DIR}/etc/dunst'
 
-UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-APPIMAGETOOL="https://github.com/pkgforge-dev/appimagetool-uruntime/releases/download/continuous/appimagetool-$ARCH.AppImage"
+# Deploy dependencies
+quick-sharun /usr/bin/dunst* /etc/dunst
 
-# CREATE DIRECTORIES
-mkdir ./AppDir
-cd ./AppDir
+# Additional changes can be done in between here
 
-# AppRun
-echo '#!/bin/sh
-CURRENTDIR="$(dirname "$(readlink -f "$0")")"
-export PATH="$CURRENTDIR/usr/bin:$PATH"
-[ -n "$APPIMAGE" ] || APPIMAGE="$0"
-BIN="${ARGV0#./}"
-unset ARGV0
-
-# Change the current working dir since we have relative path to sysconfig file
-cd "$CURRENTDIR"
-
-if [ -f "$CURRENTDIR/usr/bin/$BIN" ]; then
-	if [ "$BIN" = "dunstctl" ]; then
-		exec "$CURRENTDIR/usr/bin/$BIN" "$@"
-	else
-		exec "$CURRENTDIR/ld-musl.so" "$CURRENTDIR/usr/bin/$BIN" "$@"
-	fi
-elif [ "$1" = "--notify" ]; then
-	shift
-	exec "$CURRENTDIR/ld-musl.so" "$CURRENTDIR"/usr/bin/dunstify "$@"
-elif [ "$1" = "--ctl" ]; then
-	shift
-	exec "$CURRENTDIR"/usr/bin/dunstctl "$@"
-else
-	if [ -z "$1" ]; then
-		echo "AppImage commands:"
-		echo " \"$APPIMAGE\"            runs dunst"
-		echo " \"$APPIMAGE --notify\"   runs dunstify"
-		echo " \"$APPIMAGE --ctl\"      runs dunstctl"
-		echo "You can also make and run symlinks to the AppImage with the names"
-		echo "dunstify and dunstctl to launch them automatically without extra args"
-		echo "running dunst..."
-	fi
-	exec "$CURRENTDIR/ld-musl.so" "$CURRENTDIR/usr/bin/dunst" "$@"
-fi' > ./AppRun
-chmod +x ./AppRun
-
-# Dummy Icon & Desktop
-touch ./dunst.png ./.DirIcon # No official icon?
-echo '[Desktop Entry]
-Type=Application
-Name=dunst
-Icon=dunst
-Exec=dunst
-Categories=System
-Hidden=true' > dunst.desktop
-
-# DEPLOY ALL LIBS
-mkdir -p ./usr/lib ./usr/bin ./local/etc/xdg
-
-cp -vr /usr/local/etc/xdg/dunst   ./local/etc/xdg
-cp -v /usr/local/bin/dunst*       ./usr/bin
-cp -v /usr/lib/libnotify.so*      ./usr/lib
-cp -v /lib/ld-musl-*.so.1         ./ld-musl.so
-
-ldd ./usr/bin/* ./usr/lib/* \
-	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vf {} ./usr/lib
-
-find ./usr -type f -exec patchelf --set-rpath '$ORIGIN:$ORIGIN/../lib' {} ';'
-find ./usr -type f -exec strip -s -R .comment --strip-unneeded {} ';'
-
-# Fix hardcoded path to sysconfig file
-sed -i 's|/usr/local/etc|././/local/etc|g' ./usr/bin/*
-
-export VERSION="$(./AppRun --version | awk '{print $(NF-1); exit}')"
-[ -n "$VERSION" ]
-echo "$VERSION" > ~/version
-
-# Do the thing!
-cd ..
-wget "$APPIMAGETOOL" -O appimagetool
-chmod +x ./appimagetool
-URUNTIME_PRELOAD=1 \
-	./appimagetool -n -u \
-	"$UPINFO" ./AppDir ./dunst-"$VERSION"-anylinux-"$ARCH".AppImage
-
-echo "All Done!"
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
