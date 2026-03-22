828744c3bf0d0edbe33f50b9fc370ca5584e437f
Samueru-sama <xdglawyer@outlook.com>
2026-03-21 22:48:38 -0400

    update to latest template

--- make-appimage.sh
+++ make-appimage.sh
@@ -23,3 +23,7 @@ quick-sharun \
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --simple-test ./dist/*.AppImage
